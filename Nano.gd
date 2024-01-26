53eda42f7f108aff78b3bc48df21a82527916875
Emceachin <ericmcea@uw.edu>
2024-01-25 18:58:00 -0800

    Title screen

--- Nano.gd
+++ Nano.gd
@@ -23,3 +23,7 @@ func _on_timer_timeout():
 func _on_player_health_depleted():
 	%GameOver.visible = true
 	get_tree().paused = true
+
+
+func _on_start_o_ver_pressed():
+	get_tree().reload_current_scene()
