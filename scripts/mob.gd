708153fb7f7e9625fa56500d00483ba2e9d89128
mark-on-gh <mjoshuahua@gmail.com>
2024-02-15 22:45:24 -0800

    update scrub enemy

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,9 +1,6 @@
 extends CharacterBody2D
 
-var health = 1
-
 @onready var player = get_node("/root/Game/Player")
-
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
@@ -11,10 +8,8 @@ func _physics_process(delta):
 	move_and_slide()
 
 func take_damage():
-	health -= 1
-	if health == 0:
-		queue_free()
-		const SMOKE_EXPLOSION = preload("res://scenes/smoke_explosion.tscn")
-		var smoke = SMOKE_EXPLOSION.instantiate()
-		get_parent().add_child(smoke)
-		smoke.global_position = global_position
+	queue_free()
+	const SMOKE_EXPLOSION = preload("res://scenes/smoke_explosion.tscn")
+	var smoke = SMOKE_EXPLOSION.instantiate()
+	get_parent().add_child(smoke)
+	smoke.global_position = global_position
