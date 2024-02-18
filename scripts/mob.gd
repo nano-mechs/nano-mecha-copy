88818e96751c01edbb8fc36f5bb8e58f65fd270a
mark-on-gh <mjoshuahua@gmail.com>
2024-02-18 13:24:26 -0800

    geenralize enemy target

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@onready var player = get_node("/root/Game/Player")
+# value assigned(usually the player) on spawn from the parent scene
+var target
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
+	var direction = global_position.direction_to(target.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
 
