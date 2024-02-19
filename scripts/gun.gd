b078329ab84a206c61bf3b5232511372f15da7ef
mark-on-gh <mjoshuahua@gmail.com>
2024-02-18 18:28:29 -0800

    update gun.gd for less specific use

--- scripts/gun.gd
+++ scripts/gun.gd
@@ -1,17 +1,9 @@
 extends Area2D
 
-
-func _physics_process(delta: float ) -> void:
-	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("Shoot"):
-		shoot()
-
-	
+const BULLET = preload("res://scenes/bullet.tscn")
 
 func shoot():
-	const BULLET = preload("res://scenes/bullet.tscn")
 	var new_bullet = BULLET.instantiate()
-	new_bullet.global_position =%ShootingPoint.global_position
-	new_bullet.global_rotation =%ShootingPoint.global_rotation
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
-	
