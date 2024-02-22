7c533576053c25267f25c1059d39b68182fe7639
mark-on-gh <mjoshuahua@gmail.com>
2024-02-21 19:59:24 -0800

    update state.gd

--- scripts/state.gd
+++ scripts/state.gd
@@ -1,10 +1,15 @@
-# GLOBAL STUFF THAT NEEDS TO MAINTAIN STATE PASSED BETWEEN SCENES GO HERE
+# Global scripts that contains variables that require state to be kept when switching scenes
+# TODO: add more levels/randomize/difficulty ramp up
+# TODO: make game a game
 extends Node
 
+# global vars accessed via State.<var name>
 var prev_scene = null
 var level = 1
 var lives = 3
 
+
+# returns a hash that contains the enemy types
 func assign_enemy_props():
 	var enemies = { "spawned": 0, "killed": 0 }
 
@@ -20,3 +25,11 @@ func assign_enemy_props():
 			]
 
 	return enemies
+
+# returns the string for the current level's background sprite
+func assign_bg_sprite():
+	match level:
+		1: return "res://sprites/backgrounds/ExK_q-IVcAUM7Qu.png"
+		2: return "res://sprites/backgrounds/pixel-art-night-city-seamless-background-detailed-vector-23730672.png"
+
+
