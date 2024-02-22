4a806544501faa222cd54950d7d7bbde3c9cf598
mark-on-gh <mjoshuahua@gmail.com>
2024-02-21 22:54:20 -0800

    fix mob2

--- scripts/mob2.gd
+++ scripts/mob2.gd
@@ -1,18 +1,20 @@
 extends CharacterBody2D
 
-var health = 3
+signal killed
 
-@onready var player = get_node("/root/Game/Player")
+var health = 3
+var target
 
 
 func _physics_process(delta):
-	var direction = global_position.direction_to(player.global_position)
+	var direction = global_position.direction_to(target.global_position)
 	velocity = direction * 300.0
 	move_and_slide()
 
 func take_damage():
 	health -= 1
 	if health == 0:
+		killed.emit()
 		queue_free()
 		const SMOKE_EXPLOSION = preload("res://scenes/smoke_explosion.tscn")
 		var smoke = SMOKE_EXPLOSION.instantiate()
