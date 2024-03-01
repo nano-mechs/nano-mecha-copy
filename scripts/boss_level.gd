7d2003bc9c79b119db591a07507f163f5c0fd7e0
mark-on-gh <mjoshuahua@gmail.com>
2024-02-29 23:32:25 -0800

    remove unused files

--- scripts/boss_level.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-
-func _on_player_killed():
-	State.prev_scene = get_tree().current_scene.scene_file_path
-	get_tree().change_scene_to_file("res://scenes/game_over_screen.tscn")
-
-func _on_boss_killed():
-	get_tree().change_scene_to_file("res://scenes/transition_screen.tscn")
