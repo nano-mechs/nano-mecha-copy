44f4d7becc605163a7e9bd105d07784fec926aa1
mark-on-gh <mjoshuahua@gmail.com>
2024-03-03 01:24:19 -0800

    add empty wall damage

--- scripts/play_game_walls.gd
+++ scripts/play_game_walls.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+# removes problem with player asking for damage on wall contact
+var damage = 0
 # removes the need for an if check when a bullet hits a wall
 func take_damage(_damage = null):
 	pass
