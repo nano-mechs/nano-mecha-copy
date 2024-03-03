20dc0be8cf6174e464a1e98d8e3a7322eb36bda0
mark-on-gh <mjoshuahua@gmail.com>
2024-03-03 00:37:15 -0800

    misc fix

--- weapons/gun.gd
+++ weapons/gun.gd
@@ -5,4 +5,5 @@ var bullet = preload("res://weapons/bullet.tscn")
 func shoot():
 	var new_bullet = bullet.instantiate()
 	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
 	%ShootingPoint.add_child(new_bullet)
