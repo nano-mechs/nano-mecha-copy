1ab298dd36d663a07eb49d6b31f4b910a609bf3c
mark-on-gh <mjoshuahua@gmail.com>
2024-03-02 23:18:10 -0800

    change level one mob

--- scripts/play_game_state.gd
+++ scripts/play_game_state.gd
@@ -13,7 +13,7 @@ func assign_enemy_props():
 	match level:
 		1:
 			enemies.max_spawn_count = 10
-			enemies.types = [ preload("res://mobs/ranged.tscn") ]
+			enemies.types = [ preload("res://mobs/mob.tscn") ]
 		2:
 			enemies.max_spawn_count = 20
 			enemies.types = [
