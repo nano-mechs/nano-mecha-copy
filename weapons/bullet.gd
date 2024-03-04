c3be38dda4ec7a4ac2f50f2869198ee2294d8563
mark-on-gh <mjoshuahua@gmail.com>
2024-03-03 17:07:47 -0800

    remove bullet range

--- weapons/bullet.gd
+++ weapons/bullet.gd
@@ -1,18 +1,13 @@
 extends Area2D
 
 var travelled_distance = 0
-var speed = 1000
-var range = 1200
+var speed = 2000
 var damage = 1
 
 
 func _physics_process(delta):
 	var direction = Vector2.RIGHT.rotated(rotation)
 	position += direction * speed * delta
-
-	travelled_distance += speed * delta
-	if travelled_distance > range:
-		queue_free()
 
 
 func _on_body_entered(body):
