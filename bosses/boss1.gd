9dc2dcba3393ba998140ede471514fbd4978cb95
mark-on-gh <mjoshuahua@gmail.com>
2024-03-05 17:33:54 -0800

    update boss1 fight mechanics

--- bosses/boss1.gd
+++ bosses/boss1.gd
@@ -4,15 +4,20 @@ extends CharacterBody2D
 
 # EVERY MOB SHOULD HAVE A KILLED SIGNAL
 signal killed
+
+var target
 var health = 100
-var speed = 65
-var damage = 4
+var speed = 5
+var speed_up = 5
+var damage = 5
+var child = preload("res://mobs/teleporter.tscn")
 
 func _physics_process(delta):
 	%MoveProgress.progress += speed
 	# DO NOT CHANGE TO %MoveProgress.global_position
 	# IT JUST WORKS LIKE THIS, I DON'T KNOW WHY
 	global_position = %MoveProgress.position
+
 
 func take_damage(damage = 1):
 	health -= damage
@@ -23,3 +28,11 @@ func take_damage(damage = 1):
 		const SMOKE_EXPLOSION = preload("res://scenes/smoke_explosion.tscn")
 		var smoke = SMOKE_EXPLOSION.instantiate()
 		get_parent().add_child(smoke)
+
+
+func _on_timer_timeout():
+	speed += speed_up
+	var new_child = child.instantiate()
+	new_child.global_position = global_position
+	new_child.target = target
+	get_parent().add_child(new_child)
