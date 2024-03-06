1b671fdf5eb981f2fa6517169496da2dc288b056
mark-on-gh <mjoshuahua@gmail.com>
2024-03-05 17:55:19 -0800

    remove unused files

--- test/enemy.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-signal killed
-
-func _on_button_pressed():
-	killed.emit()
-	queue_free()
--- test/player.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-signal killed
-
-func _physics_process(delta):
-	velocity = 600 * Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	move_and_slide()
